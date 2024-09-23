156e063e7d4bf293c5c22c1c90a3adb1cf486959
MCRE-BE <mathias.creemers@gmail.com>
2024-09-23 20:13:23 +0200

    [FEAT] Initial Commit for project setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup of the pacakge.
+
+Simple setup file based on the input provided by documentation.
+
+Notes
+-----
+https://packaging.python.org/en/latest/guides/modernize-setup-py-project/#should-setup-py-be-deleted
+"""
+
+import setuptools
+
+setuptools.setup()
